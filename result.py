c781810000aa58dfa6a49cbbcd4aec0ab10b0ec6
Abhay Ruparel <abhayruparel2000@gmail.com>
2022-01-11 21:44:29 +0530

    final

--- result.py
+++ result.py
@@ -8,29 +8,33 @@ import os
 def generateImage(filename):
     driver = webdriver.Chrome()
     chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(options=chrome_options)
     url = "https://result.ganpatuniversity.ac.in/"
 
     driver.get(url)
 
     # institute
-    driver.find_element_by_name('ddlInst').click()
+    driver.find_element_by_name("ddlInst").click()
     driver.find_element_by_xpath(
-        '/html/body/form/div[3]/table[2]/tbody/tr[1]/td[2]/select/option[18]').click()
+        "/html/body/form/div[3]/table[2]/tbody/tr[1]/td[2]/select/option[18]"
+    ).click()
 
     # Degree
-    driver.find_element_by_name('ddlDegree').click()
+
+    driver.find_element_by_name("ddlDegree").click()
     driver.find_element_by_xpath(
-        '/html/body/form/div[3]/table[2]/tbody/tr[2]/td[2]/select/option[3]').click()
+        "/html/body/form/div[3]/table[2]/tbody/tr[2]/td[2]/select/option[3]"
+    ).click()
 
     # Sem
-    driver.find_element_by_name('ddlSem').click()
+    driver.find_element_by_name("ddlSem").click()
     driver.find_element_by_xpath(
-        '/html/body/form/div[3]/table[2]/tbody/tr[3]/td[2]/select/option[6]').click()
+        "/html/body/form/div[3]/table[2]/tbody/tr[3]/td[2]/select/option[6]"
+    ).click()
     # Exam
-    driver.find_element_by_name('ddlScheduleExam').click()
+    driver.find_element_by_name("ddlScheduleExam").click()
     ssName = filename
     ss = driver.save_screenshot(ssName)
 
@@ -46,12 +50,12 @@ def compare():
     if hashVal[1] != newHashVal[1]:
         # remove mScreenShot and rename nScreenShot.png as mScreenshot.png
         print("Some Update found")
-        os.remove('mScreenShot.png')
-        os.rename('nScreenShot.png', 'mScreenShot.png')
+        os.remove("mScreenShot.png")
+        os.rename("nScreenShot.png", "mScreenShot.png")
         # sendMail()
     else:
         print("No changes found.\nDeleting nScreenShot.png")
-        os.remove('nScreenShot.png')
+        os.remove("nScreenShot.png")
 
 
 # driver code
@@ -60,11 +64,11 @@ if os.path.exists("mScreenShot.png"):
     # Append-adds at last
     print("mScreenShot file is existing")
     # Generating Current Screenshot
-    generateImage('nScreenShot.png')
+    generateImage("nScreenShot.png")
     print("Stored in nScreenshot.png FILE")
     compare()
 else:  # when the script is running for very first time
     print("FILE: mScreenShot.png doesn't exist")
     # Generating Current Screenshot
-    generateImage('mScreenShot.png')
+    generateImage("mScreenShot.png")
     print("Stored in mScreenShot.png FILE")
--- resultStatus.py
+++ resultStatus.py
@@ -1,5 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+import smtplib
+import mimetypes
+import os
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+
+
+def sendMail():
+
+    msg = MIMEMultipart()
+    user = os.environ.get('id') #Enter sender's email address
+    passwd = os.environ.get('pass')
+    msg['Subject'] = "Sem-V Result is now live!"
+    msg['From'] = user
+    msg['To'] = "abhayruparel2000@gmail.com, rushil.rc@gmail.com" #Enter receiver's email address
+    msgContent = "https://result.ganpatuniversity.ac.in/"
+    Content = MIMEText(msgContent,'plain')
+    msg.attach(Content)
+    with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
+        smtp.login(user,passwd)
+        smtp.send_message(msg)
+    print("E-Mail Sent!")
+
 
 mainDriver = webdriver.Chrome(executable_path='chromedriver_win32\chromedriver.exe')
 mainDriver.get('https://result.ganpatuniversity.ac.in/')
@@ -12,5 +38,6 @@ for i in examsList:
 mainDriver.close()
 if len(examsList) > 3:
     print('Result for SEM V has been declared.')
+    sendMail()
 else:
     print('Result has not yet been declared.')
