5ce01c6c6617ab679dee4428d81ec9dcc17693fe
Abhay Ruparel <abhayruparel2000@gmail.com>
2022-01-11 21:27:37 +0530

    version

--- resultStatus.py
+++ resultStatus.py
@@ -7,6 +7,8 @@ mainDriver.find_element_by_xpath("//select[@name='ddlInst']/option[text()='16 - 
 mainDriver.find_element_by_xpath("//select[@name='ddlDegree']/option[text()='B.TECH-CSE(CS)']").click()
 mainDriver.find_element_by_xpath("//select[@name='ddlSem']/option[text()='V']").click()
 examsList = Select(mainDriver.find_element_by_xpath("//select[@name='ddlScheduleExam']")).options
+for i in examsList:
+    print(i.text)
 mainDriver.close()
 if len(examsList) > 3:
     print('Result for SEM V has been declared.')
