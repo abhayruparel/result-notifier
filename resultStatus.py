fa89ebd2e0c08981c4ace13d52e8e41a569d6bc2
Abhay Ruparel <abhayruparel2000@gmail.com>
2022-01-11 21:54:00 +0530

    ...

--- resultStatus.py
+++ resultStatus.py
@@ -36,7 +36,7 @@ examsList = Select(mainDriver.find_element_by_xpath("//select[@name='ddlSchedule
 for i in examsList:
     print(i.text)
 mainDriver.close()
-if len(examsList) < 3:
+if len(examsList) <= 3:
     print('Result for SEM V has been declared.')
     sendMail()
 else:
